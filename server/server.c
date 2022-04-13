9a073d6f6ce6113816cadaf6fae2753a95f7310a
Jacob Schwartz <jschwar5@nd.edu>
2022-04-12 20:47:47 -0400

    start

---
+++ server/server.c
@@ -0,0 +1,114 @@
+void Server_Lobby (uint16_t nLobbyPort)
+{
+	// Adapting this from Beej's Guide
+	
+    int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
+    struct addrinfo hints, *servinfo, *p;
+    struct sockaddr_storage their_addr; // connector's address information
+    socklen_t sin_size;
+    struct sigaction sa;
+    int yes=1;
+    char s[INET6_ADDRSTRLEN];
+    int rv;
+	
+	struct ClientInfo theClientInfo;
+	int       nClientCount = 0;	
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE; // use my IP
+
+    if ((rv = getaddrinfo(NULL, nLobbyPort, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+
+    // loop through all the results and bind to the first we can
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                p->ai_protocol)) == -1) {
+            perror("server: socket");
+            continue;
+        }
+
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+                sizeof(int)) == -1) {
+            perror("setsockopt");
+            exit(1);
+        }
+
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            perror("server: bind");
+            continue;
+        }
+
+        break;
+    }
+
+    freeaddrinfo(servinfo); // all done with this structure
+
+    if (p == NULL)  {
+        fprintf(stderr, "server: failed to bind\n");
+        exit(1);
+    }
+
+    if (listen(sockfd, BACKLOG) == -1) {
+        perror("listen");
+        exit(1);
+    }
+
+    printf("server: waiting for connections...\n");
+
+    while(g_bKeepLooping) 
+	{  
+        sin_size = sizeof their_addr;		
+        new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+		
+        if (new_fd == -1) 
+		{
+            perror("accept");
+            continue;
+        }
+
+		/* Simple bit of code but this can be helpful to detect successful
+		   connections 
+		 */
+        inet_ntop(their_addr.ss_family,
+            get_in_addr((struct sockaddr *)&their_addr),
+            s, sizeof s);
+        printf("server: got connection from %s\n", s);
+
+		// Print out this client information 
+		sprintf(theClientInfo.szIdentifier, "%s-%d", s, nClientCount);
+		theClientInfo.socketClient = new_fd;
+		nClientCount++;
+
+		/* From OS: Three Easy Pieces 
+		 *   https://pages.cs.wisc.edu/~remzi/OSTEP/threads-api.pdf */
+		pthread_create(&(theClientInfo.threadClient), NULL, Thread_Client, &theClientInfo);
+		
+		// Bail out when the third client connects after sleeping a bit
+		if(nClientCount == 3)
+		{
+			g_bKeepLooping = 0;
+			sleep(15);
+		}		
+    }
+
+
+}
+
+int main(int argc, char *argv[]) 
+{
+	pthread_mutex_init(&g_BigLock, NULL);
+	
+	Server_Lobby(41000);
+
+	printf("Sleeping before exiting\n");
+	sleep(15);
+	
+	printf("And we are done\n");
+	return 0;
+}	
