d86098c59a1eea9530c855a99da0880fd4e83b4f
Jacob Schwartz <jschwar5@nd.edu>
2022-04-12 20:48:34 -0400

    includes server

--- server/server.c
+++ server/server.c
@@ -1,3 +1,17 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+
 void Server_Lobby (uint16_t nLobbyPort)
 {
 	// Adapting this from Beej's Guide
